e97332651ea5c7d537fdb7d3575b92e2f641f989
monir_mostafizur <mostafizur.rahman@streamstech.com>
2025-05-08 22:08:43 +0600

    rabbitmq integrate

--- NewsLetter.API/Program.cs
+++ NewsLetter.API/Program.cs
@@ -1,8 +1,12 @@
+using NewsLetter.API;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddApplicationServices(builder.Configuration); // Your DI extension method
 
 var app = builder.Build();
 
